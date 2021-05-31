9cd33088fc0ba94c0acfcf7b685c4f29f3d6622e
yehouda <bouskilayehouda@gmail.com>
2021-05-31 23:49:30 +0200

    test

--- src/main/java/com/dsp/theTipTop/SecurityConfig.java
+++ src/main/java/com/dsp/theTipTop/SecurityConfig.java
@@ -1,11 +1,9 @@
 package com.dsp.theTipTop;
 
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-import org.springframework.security.crypto.password.NoOpPasswordEncoder;
 
 
 @Configuration
@@ -17,7 +15,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 	protected void configure(HttpSecurity httpSecurity) throws Exception {
 		httpSecurity
 		 .antMatcher("/**").authorizeRequests()
-         .antMatchers(new String[]{"/", "/not-restricted"}).permitAll()
+         .antMatchers("/").permitAll()
          .anyRequest().authenticated()
          .and().oauth2Login()
          .defaultSuccessUrl("/users/oauth2LoginSuccess")
