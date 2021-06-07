6c3217f2ac3050297a76f54339ca479ee06df7e9
yehouda <bouskilayehouda@gmail.com>
2021-06-07 22:02:57 +0200

    change pom plugin

--- src/main/java/com/dsp/theTipTop/configurations/SecurityConfig.java
+++ src/main/java/com/dsp/theTipTop/configurations/SecurityConfig.java
@@ -1,4 +1,4 @@
-package com.dsp.theTipTop;
+package com.dsp.theTipTop.configurations;
 
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
