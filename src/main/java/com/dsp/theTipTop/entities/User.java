1211d747c0276581568088cbaab89d8d4ca13be5
yehouda <bouskilayehouda@gmail.com>
2021-08-11 23:22:34 +0200

    add @Column(unique = true)

--- src/main/java/com/dsp/theTipTop/entities/User.java
+++ src/main/java/com/dsp/theTipTop/entities/User.java
@@ -2,6 +2,7 @@ package com.dsp.theTipTop.entities;
 
 import java.util.Date;
 
+import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
@@ -11,6 +12,8 @@ import javax.persistence.OneToOne;
 import javax.persistence.Temporal;
 import javax.persistence.TemporalType;
 import javax.validation.constraints.NotNull;
+
+import org.hibernate.validator.constraints.UniqueElements;
 
 import lombok.AllArgsConstructor;
 import lombok.Data;
@@ -25,7 +28,7 @@ public class User {
 	@Id @GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
 	
-	@NotNull
+	@NotNull @Column(unique = true)
 	private String email;
 	
 	private String firstName;
