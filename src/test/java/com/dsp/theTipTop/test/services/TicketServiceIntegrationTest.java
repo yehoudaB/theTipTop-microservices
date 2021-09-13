8929ecb47bdb9963f3b7e790b5dd2e740c9598c4
yehouda <bouskilayehouda@gmail.com>
2021-09-13 17:14:35 +0200

    fix 1 test

--- src/test/java/com/dsp/theTipTop/test/services/TicketServiceIntegrationTest.java
+++ src/test/java/com/dsp/theTipTop/test/services/TicketServiceIntegrationTest.java
@@ -3,12 +3,16 @@ package com.dsp.theTipTop.test.services;
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
 
+import java.util.Random;
+import java.util.concurrent.ThreadLocalRandom;
+
 import org.junit.jupiter.api.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.context.SpringBootTest.WebEnvironment;
 import org.springframework.http.ResponseEntity;
+import org.springframework.test.context.TestPropertySource;
 import org.springframework.test.context.junit4.SpringRunner;
 
 import com.dsp.theTipTop.entities.Ticket;
@@ -16,7 +20,7 @@ import com.dsp.theTipTop.services.TicketService;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest(webEnvironment = WebEnvironment.NONE) 
-//@TestPropertySource(locations = "/application-local.properties") //for local test
+@TestPropertySource(locations = "/application-local.properties") //for local test
 class TicketServiceIntegrationTest {
 
 	@Autowired
@@ -26,7 +30,8 @@ class TicketServiceIntegrationTest {
 	@Test
 	void testAddTicket() {
 		Ticket ticket  = new Ticket();
-		ticket.setTicketId((long) 123);
+		int randomNum = ThreadLocalRandom.current().nextInt(1, 99999 + 1);
+		ticket.setTicketId((long) randomNum);
 		ticket.setName("Ticket de test 1");
 		ticket.setHadTheGift(true);
 		
