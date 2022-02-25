77ea3bb79d5dadf2385fdf11828e7e85f51d9652
yehoudaB <bouskilayehouda@gmail.com>
2022-02-25 01:13:17 +0100

    change deleteuser

--- src/main/java/com/dsp/theTipTop/repositories/UserRepository.java
+++ src/main/java/com/dsp/theTipTop/repositories/UserRepository.java
@@ -3,6 +3,8 @@ package com.dsp.theTipTop.repositories;
 import java.util.Optional;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 import com.dsp.theTipTop.entities.User;
 
@@ -11,4 +13,6 @@ public interface UserRepository extends JpaRepository<User, Long> {
 
 	Optional<User> findByEmail(String email);
 	
+	@Query("DELETE FROM user WHERE user.id = :id")
+	void deleteById(@Param("id") Long id);
 }
